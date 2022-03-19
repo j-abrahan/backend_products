c57ee02a260c25002ed5aaad4b2e77edd4ded71f
Abrahan <you@example.com>
2022-03-19 15:47:05 +0100

    Enable token authentication by installing proper rest_framework app and enabling the corresponding url to get the token back

--- backend_products/urls.py
+++ backend_products/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.urls import path, re_path, include
 from django.contrib import admin
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    re_path(r'admin/', admin.site.urls),
     re_path(r'^', include('products.urls')),
+    re_path(r'^api-token-auth/', obtain_auth_token),
 ]
