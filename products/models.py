39a7d27a5c2821efc91b75c5aa50683737f31b13
Abrahan <you@example.com>
2022-03-19 14:03:14 +0100

    Added Product model and enabling the app in the settings file

--- products/models.py
+++ products/models.py
@@ -1,3 +1,7 @@
+from __future__ import unicode_literals
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=40, unique=True)
+    price = models.DecimalField(decimal_places=2, max_digits=20)
+    quantity = models.IntegerField()
